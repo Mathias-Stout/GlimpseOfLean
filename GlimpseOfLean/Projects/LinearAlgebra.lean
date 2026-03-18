e5f63d13c5fab4450460fa6de1c29630b9cf9d81
Mathias Stout <mathias.stout@hotmail.com>
2026-03-18 12:12:30 -0400

    suggested project in LinAlg

---
+++ GlimpseOfLean/Projects/LinearAlgebra.lean
@@ -0,0 +1,8 @@
+/- In `Mathlib.LinearAlgebra.Matrix.Transvection` it is proven that one can reduce a matrix
+ to a diagonal form by only performing row and column operations (and without ever permuting them).
+
+ State the corresponding theorem for row-echelon form instead of diagonal form.
+ Start by defining row-echelon form and the sets of allowed transformations.
+ If you feel particularly brave, you may attempt to prove (parts) of this theorem,
+ drawing inspiration from `Mathlib.LinearAlgebra.Matrix.Transvection`.
+ -/
